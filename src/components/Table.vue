bc3407787b6cd005d31fe1d602fdd552ad02ca1b
1704mori <1704mori@gmail.com>
2024-01-30 19:15:44 -0300

    add table and pagination components

---
+++ src/components/Table.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="flex flex-col gap-1">
+    <div
+      class="space-y-2 md:space-y-0 bg-neutral-50 dark:bg-neutral-950 border border-neutral-200 dark:border-neutral-800 rounded-lg divide-y divide-neutral-200 dark:divide-neutral-800 overflow-hidden max-h-80 overflow-y-scroll"
+    >
+      <div
+        v-if="!totalData || !data.length"
+        class="flex justify-between items-center px-4 py-2 bg-neutral-50 dark:bg-neutral-950"
+      >
+        <p class="text-sm font-medium">no results found</p>
+      </div>
+
+      <div
+        v-for="(item, index) in data as any"
+        :key="index"
+        :class="[
+          'bg-neutral-50 dark:bg-neutral-950 px-1 py-1.5 shadow-md flex flex-wrap [@media(min-width:840px)]:grid sm:gap-4',
+          className,
+        ]"
+        :style="{ gridTemplateColumns: generateGridColumns.join(' ') }"
+      >
+        <div
+          v-for="(column, columnIndex) in columns as any"
+          :key="columnIndex"
+          :class="[
+            'w-full p-2 mt-auto',
+            {
+              'max-md:[&:not(:last-child)]:border-b border-neutral-200 dark:border-neutral-800 self-center truncate':
+                (typeof item[column.key] === 'string' &&
+                  !isHTMLElement(item[column.key])) ||
+                isHTMLElement(item[column.key]),
+              'self-center':
+                typeof item[column.key] === 'object' &&
+                isHTMLElement(item[column.key]) &&
+                item[column.key].props?.className,
+            },
+          ]"
+        >
+          <p
+            v-if="typeof item[column.key] == 'string'"
+            class="font-medium"
+            v-html="item[column.key]"
+          />
+          <div
+            v-if="
+              column.key === 'actions' && typeof item[column.key] != 'string'
+            "
+            class="w-full flex justify-center items-center"
+          >
+            <component :is="item[column.key]" />
+          </div>
+
+          <div
+            v-if="
+              typeof item[column.key] !== 'string' &&
+              typeof item[column.key] == 'object'
+            "
+            :class="[
+              'flex flex-col justify-start items-stretch relative min-w-full',
+              item[column.key].rules,
+            ]"
+          >
+            <p class="text-sm font-semibold">{{ item[column.key].head }}</p>
+            <p class="text-sm truncate">
+              {{
+                column.modifyValue
+                  ? column.modifyValue(item[column.key].child)
+                  : item[column.key].child
+              }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs } from "vue";
+import Pagination from "@/components/Pagination.vue";
+
+export default defineComponent({
+  name: "Table",
+  components: {
+    Pagination,
+  },
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+    columns: {
+      type: Array,
+      required: true,
+    },
+    className: String,
+    rowsPerPage: {
+      type: Number,
+      default: 5,
+    },
+    totalData: Number,
+    onPageClick: Function,
+  },
+  setup(props) {
+    const { columns } = toRefs(props);
+
+    // find a better way to check if the value is an HTML element
+    const isHTMLElement = (value: any) => {
+      const htmlTags = [
+        "input",
+        "button",
+        "select",
+        "textarea",
+        "div",
+        "p",
+        "span",
+      ];
+      return htmlTags.includes(value.type);
+    };
+
+    const generateGridColumns = columns.value.map((column: any) =>
+      ["id", "actions"].includes(column.key) ? "auto" : "1fr",
+    );
+
+    return {
+      isHTMLElement,
+      generateGridColumns,
+    };
+  },
+  methods: {
+    pageChanged(page: number) {
+      if (this.onPageClick) {
+        this.onPageClick(page);
+      }
+    },
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,29 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <main>
     ayo
-  </main>
+     </main>
 </template>
+
+<script setup lang="ts">
+import Table from "@/components/Table.vue" 
+
+const columns = ref([
+  { key: "id", label: "ID" },
+  { key: "name", label: "Name" },
+  { key: "status", label: "Status" },
+  { key: "image", label: "Image" },
+  { key: "ports", label: "Ports" },
+  { key: "created_at", label: "Created At" },
+]);
+const data = [
+  {
+    id: 123,
+    name: "alo",
+    status: "eae",
+    image: "koe",
+    ports: "321",
+    created_at: "2024-01-15",
+  },
+]
+const total = data.length;
+</script>
