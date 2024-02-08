b01e6aa5d09fc409849970157078fa13aa13bb7b
1704mori <1704mori@gmail.com>
2024-02-08 00:53:23 -0300

    Update Droplet color in App.vue

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,11 @@ const toggleDark = useToggle(isDark);
       class="bg-neutral-50 dark:bg-neutral-950 border border-neutral-200 dark:border-neutral-900 hover:bg-neutral-100 dark:hover:bg-neutral-900 p-2 rounded-full transition-all active:scale-95"
       @click="toggleDark()"
     >
-      <Droplet />
+      <Droplet class="text-black dark:text-white" />
     </button>
   </div>
 
-  <main class="w-full">
+  <main class="w-full text-neutral-900 dark:text-neutral-100">
     <RouterView />
   </main>
 </template>
